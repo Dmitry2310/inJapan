95eba6cab8e1a0dd388decc25deb388b23b4aec6
Dmitry <dmitriy231087@gmail.com>
2023-04-04 20:35:15 +0900

    add build script

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseURL = 'https://injapan-production.up.railway.app';
+export const baseURL = 'https://injapan.onrender.com';
 
 const API = axios.create({
     baseURL
